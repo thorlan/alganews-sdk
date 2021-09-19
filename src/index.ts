7969f0f72c5d1ee856962416b764f54377136191
thorlan <thiagorlandini@hotmail.com>
2021-09-19 15:25:43 -0300

    maypment para payroll

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export { default as FileService } from './services/File.service'
 export { default as MetricService } from './services/Metric.service'
 export { default as UserService } from './services/User.service'
 export { default as PostService } from './services/Post.service'
-export { default as PaymentService } from './services/Payment.service'
+export { default as PayrollService } from './services/Payroll.service'
 export { default as CashFlowService } from './services/CashFlow.service'
 
 export { default as generateQueryString } from './utils/generateQueryString'
