9a2bb1532b2d3486ad9e1ab0c2ba418d2b330d1e
thorlan <thiagorlandini@hotmail.com>
2021-09-19 15:34:29 -0300

    metric

--- src/services/Metric.service.ts
+++ src/services/Metric.service.ts
@@ -11,6 +11,33 @@ class MetricService extends Service {
     static getEditorMonthlyEarnings(){
         return this.Http.get<Metric.EditorMonthlyEarnings>('/metrics/editor/monthly-earnings').then(this.getData);
     }
+
+    static getMonthlyRevenuesExpensesChartJs() {
+
+        return this.Http.get<Metric.MonthlyRevenuesExpensesChartJs>(
+          '/metrics/monthly-revenues-expenses',
+          {
+            headers: {
+              'Content-Type':
+                'application/vnd.alganews.chartjs+json',
+            },
+          }
+        ).then(this.getData);
+
+      }
+      
+      static getMonthlyRevenuesExpenses() {
+
+        return this.Http.get<Metric.MonthlyRevenuesExpenses>(
+          '/metrics/monthly-revenues-expenses',
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        ).then(this.getData);
+        
+      }
 }
 
 export default MetricService;
